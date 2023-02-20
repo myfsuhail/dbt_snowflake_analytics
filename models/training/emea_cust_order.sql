4fcdebe38fbc12376ae7c3ab39068c9aa61e7036
snowflake800000@gmail.com <snowflake800000@gmail.com>
2023-02-20 14:47:32 +0000

    Commit

---
+++ models/training/emea_cust_order.sql
@@ -0,0 +1,19 @@
+{{config (
+    schema = 'EMEA', 
+    database = 'DEV',
+    materialized = 'table'
+)}}
+
+select
+	a.cust_key,
+    b.o_orderkey as order_key,
+    a.cust_nm,
+    a.nation,
+    a.region_name,
+    a.cust_acct_bal,
+    O_ORDERSTATUS as order_status_cd,
+    O_ORDERDATE as order_dt,
+    O_TOTALPRICE as total_price
+from {{ref('emea_customers')}} a
+inner join {{source('PUBLIC','ORDERS')}} b
+on a.cust_key=b.o_custkey
