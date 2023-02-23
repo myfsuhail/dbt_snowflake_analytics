10ac4d194e8d51062554bf08acdc3cdda3fe2989
snowflake800000@gmail.com <snowflake800000@gmail.com>
2023-02-23 07:55:14 +0000

    Included macros

---
+++ macros/get_max_upd_ts.sql
@@ -0,0 +1,20 @@
+{% macro get_max_upd_ts() %}
+
+{% set sql_statement %}
+
+        select nvl(max(upd_ts),'1970-01-01') from {{this}};
+
+{% endset %}
+
+{%- set max_time = run_query(sql_statement) -%}
+
+{% if execute %}
+      {% if max_time %}
+        {% set result_value = max_time.columns[0].values()[0] %}
+          {{ return (result_value) }}                  
+      {% else %}
+          {{ return ('1900-01-01') }}  
+      {% endif %}
+{% endif %}
+
+{%- endmacro -%}
