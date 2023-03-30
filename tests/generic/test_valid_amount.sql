baf30090d7438df96acba7751e809ac1ff19f6a2
pamnanidimple@gmail.com <pamnanidimple@gmail.com>
2023-03-30 19:24:18 +0000

    added config changes

--- tests/generic/test_valid_amount.sql
+++ tests/generic/test_valid_amount.sql
@@ -1,5 +1,8 @@
 {% test test_valid_amount(model, column_name) %}
 
+{{ config(severity = 'error',
+          store_failures= true ) }}
+
     select *
     from {{ model }}
     where {{ column_name }} <0
