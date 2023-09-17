f82c0d32763610049d805eea99db8a2be267c832
myfsuhail001@outlook.com <myfsuhail001@outlook.com>
2023-09-17 14:02:31 +0000

    Updated models

--- models/consumption/na_customers.sql
+++ models/consumption/na_customers.sql
@@ -12,9 +12,7 @@ select
     n_name as nation,
     r_name as region_name,
     c_acctbal as cust_acct_bal
-from dev.public.customer
-left join dev.public.nation on customer.c_nationkey = nation.n_nationkey
-left join dev.public.region on nation.n_regionkey = region.r_regionkey
-where r_name in ('ASIA','PACIFIC')
-
-
+from {{source('raw','customer')}}
+left join {{source('raw','nation')}} on customer.c_nationkey = nation.n_nationkey
+left join {{source('raw','region')}} on nation.n_regionkey = region.r_regionkey
+where r_name in ('{{ var("region_name") }}' )
