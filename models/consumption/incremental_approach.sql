7c2c97bbf2831c0f47b2b20006c102e74e969419
myfsuhail001@outlook.com <myfsuhail001@outlook.com>
2023-09-21 02:42:13 +0000

    Commit

--- models/consumption/incremental_approach.sql
+++ models/consumption/incremental_approach.sql
@@ -1,6 +1,8 @@
 {{config (
-    materialized = 'incremental'
+    materialized = 'table'
 )}}
+
+{% set time_val = get_max_upd_ts() %}
 
 select
     c_custkey as cust_key,
@@ -9,10 +11,10 @@ select
     c_phone as cust_phone_num,
     n_name as nation,
     r_name as region_name,
-    c_acctbal as cust_acct_bal
+    c_acctbal as cust_acct_bal,
+    customer.upd_ts as src_upd_ts,
+    current_timestamp() as upd_ts 
 from {{ source ('raw','customer') }}
 left join {{ source ('raw','nation') }} on customer.c_nationkey = nation.n_nationkey
 left join {{ source ('raw','region') }} on nation.n_regionkey = region.r_regionkey
 where r_name in ('ASIA','PACIFIC')
-
-
